5d543d295793bb1a2620436b8b18448f8487efc7
Gideon33967 <109658192+Gideon33967@users.noreply.github.com>
2025-11-23 15:29:38 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,6 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
-```
-
-4. Click **"Commit new file"**
-
----
-
-### **FILE 5: .gitignore**
-
-1. Click **"Add file"** → **"Create new file"**
-2. **Filename:** `.gitignore`
-3. **Paste this:**
-```
-node_modules
-.next
-out
-.DS_Store
-*.log
-.env*.local
-.vercel
+plugins: {
+tailwindcss: {},
+autoprefixer: {},
+},
+};
